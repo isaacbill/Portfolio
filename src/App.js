23eec61e149799e3658dba112aba39c760a61104
isaac bill <isaacbill247@gmail.com>
2024-06-26 13:20:18 +0300

    testimonials done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Banner from "./components/banner/Banner";
 import Features from "./components/features/Features";
 import Projects from "./components/projects/Projects";
 import Resume from "./components/resume/Resume";
+import Testimonial from "./components/testimonial/Testimonial";
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
@@ -13,6 +14,7 @@ function App() {
         <Features />
         <Projects/>
         <Resume />
+        <Testimonial/>
       </div>
     </div>
   );
