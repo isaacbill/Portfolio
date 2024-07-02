2f60170635fe8112197f9187aa8e7b23efd5ced7
isaac bill <isaacbill247@gmail.com>
2024-07-02 17:48:08 +0300

    resume update

--- src/components/resume/Achievement.js
+++ src/components/resume/Achievement.js
@@ -10,54 +10,60 @@ const Achievement = () => {
       className="py-12 font-titleFont flex gap-20"
     >
       <div>
-        <div className="py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2007 - 2010</p>
+        <div className="flex flex-col gap-4 w-full">
+          <p className="text-sm text-designColor tracking-[4px]">2022 - 2024</p>
           <h2 className="text-4xl font-bold">Company Experience</h2>
         </div>
-        <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
+            title="Engineering Structural Design"
+            subTitle="Enhanced interactive structural design and analysis of framed structures...."
             result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+            des={`During my internship at Meijer Engineers, I gained valuable skills in Steel and Reinforced Concrete 
+            I performed structural analysis using software like Midas Gen, Tekla Tedds, Prokon and Tekla TSD,
+            designed various structural elements, and ensured compliance with industry standards.\n
+            I developed proficiency in material selection, connection design,\n
+            I also learned design codes of practice (British and EURO codes), concrete mix design, reinforcement 
+            detailing, and quality control methods.\nAdditionally, I enhanced my project management, technical 
+            documentation, and problem-solving skills, ensuring regulatory and safety compliance.This experience
+            provided a solid foundation in structural engineering and practical industry knowledge.`}
           />
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
+            title="Architectural and Structural Drawings Production"
+            subTitle="Developed Skill Report Writing and Documentation"
             result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+            des={`I developed the skill to create precise and detailed blueprints, 3D models, and 
+            Understanding software interfaces at Meijer Engineers, utilizing various commands and functions, and implementing industry
+            standards were key competencies I acquired.\nMy previous experience enhanced my ability to translate 
+            conceptual designs into accurate digital representations efficiently.\nI also honed my problem-solving 
+            skills to address design challenges, ensured compliance with building codes, and improved communication 
+            through clear, professional drawings.\nMastering these skills led to increased productivity and the production 
+            of high-quality architectural documentation.`}
           />
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
+            title="Client Satisfaction"
+            subTitle="99% Delivery On-time delivery Record"
             result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+            des="Throughout my work experience, I adhered to client design specification and delivered on time as per the code requirement, significantly contributing to client satisfaction."
           />
         </div>
       </div>
       <div>
-        <div className="py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2007 - 2010</p>
+        <div className="flex flex-col gap-4 w-full">
+          <p className="text-sm text-designColor tracking-[4px]">2022 - 2024</p>
           <h2 className="text-4xl font-bold">Job Experience</h2>
         </div>
-        <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
+            title="Project Management"
+            subTitle="Quantities estimation."
             result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
-          <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
-          />
-          <ResumeCard
-            title="Lorem ipsum dolor sit amet."
-            subTitle="Lorem ipsum dolor sit amet alternative."
-            result="Success"
-            des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora itaque accusamus corporis dolor debitis natus sapiente, voluptates possimus minima totam!"
+            des={`At Edermann, I developed expertise in accurately assessing project needs, calculating the required workforce,
+            and estimating material quantities.\nThis involved analyzing blueprints, specifications, and project scopes to 
+            create detailed and precise estimates. I employed industry-standard methodologies and tools to ensure accuracy
+            and efficiency. My ability to forecast costs, manage resources, and align estimates with budgets contributed to
+            successful project planning and execution.\nThis skill set enhanced project timelines, controlled expenses, and 
+            improved overall project outcomes, demonstrating my proficiency in project management and estimation.`}
           />
         </div>
       </div>
--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -12,27 +12,21 @@ const Education = () => {
       {/* part one */}
       <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">1998 - 2010</p>
-          <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
+          <p className="text-sm text-designColor tracking-[4px]"> - DATE</p>
+          <h2 className="text-3xl md:text-4xl font-bold">Education</h2>
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="BSc in Computer Science"
-            subTitle="University of DVI (2006 - 2010)"
+            title="BSc Civil Engineering"
+            subTitle="Machakos University (2017 - 2022)"
             result="3.90/4"
-            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
-          />
-          <ResumeCard
-            title="AS - Science & Information"
-            subTitle="SuperKing College (2001 - 2005)"
-            result="4.75/5"
-            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
-          />
+            des="The training provided me with the fundamental skills of a civil engineer,to explore more in the field of science, technology and Ecology"
+                    />
           <ResumeCard
             title="Secondary School Education"
-            subTitle="Kingstar Secondary School (1998 - 2000)"
+            subTitle="Itierio Boys High School (2013 - 2016)"
             result="5.00/5"
-            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
+            des="The training provided me with the fundamentals of STEM"
           />
         </div>
       </div>
@@ -45,22 +39,10 @@ const Education = () => {
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Sr. Software Engineer"
+            title="Project Management"
             subTitle="Google Out Tech - (2017 - Present)"
-            result="USA"
+            result="USA-remote"
             des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
-          />
-          <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
-          />
-          <ResumeCard
-            title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
-            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
           />
         </div>
       </div>
