35e9e5b9718d7f0e01e671d8b42674319bde07cd
isaac bill <isaacbill247@gmail.com>
2024-07-19 16:03:55 +0300

    responsive links

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
           </Routes>
           <Footer />
           <FooterBottom />
+          
         </div>
       </div>
     </Router>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React from 'react';
 import { FaFacebookF, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
-import {logo} from "../../assets/index"
+import { logo } from "../../assets/index";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  const handleExternalRedirect = (url) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+
   return (
     <div className="w-full py-20 h-auto border-b-[1px] border-b-black grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-4 gap-8">
       <div className="w-full h-full flex flex-col gap-8">
         <img className="w-32" src={logo} alt="logo" />
         <div className="flex gap-4">
-          <a href='https://www.facebook.com/Jared.Ongeri.Eng'target="_blank" rel="noopener noreferrer">
-          <span className="bannerIcon">
-            <FaFacebookF />
-          </span>
+          <a href='https://www.facebook.com/Jared.Ongeri.Eng' target="_blank" rel="noopener noreferrer">
+            <span className="bannerIcon">
+              <FaFacebookF />
+            </span>
           </a>
-          <a href='https://wa.me/+254701991654'target="_blank" rel="noopener noreferrer">
-          <span className="bannerIcon">
-            < FaWhatsapp />
-          </span>
+          <a href='https://wa.me/+254701991654' target="_blank" rel="noopener noreferrer">
+            <span className="bannerIcon">
+              <FaWhatsapp />
+            </span>
           </a>
-          <a href='https://www.linkedin.com/in/jared-ongeri-52970a254'target="_blank" rel="noopener noreferrer">
-          <span className="bannerIcon">
-            <FaLinkedinIn />
-          </span>
+          <a href='https://www.linkedin.com/in/jared-ongeri-52970a254' target="_blank" rel="noopener noreferrer">
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
           </a>
         </div>
       </div>
@@ -31,49 +36,42 @@ const Footer = () => {
         </h3>
         <ul className="flex flex-col gap-4 font-titleFont font-medium py-6 overflow-hidden">
           <li>
-            <a href='#'>
-            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              About
-              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
-            </span>
-            </a>
+            <Link to='/'>
+              <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+                About
+                <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+              </span>
+            </Link>
           </li>
           <li>
-          <a href='#'>
-            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              Portfolio
+            <span
+              className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer"
+              onClick={() => handleExternalRedirect('https://portfolio-isaac-okeyos-projects.vercel.app')}
+            >
+              Go to my portfolio
               <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
             </span>
-          </a>
           </li>
           <li>
-          <a href='#'>
-            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              Services
-              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
-            </span>
-          </a>
+            <Link to='/services'>
+              <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+                Services
+                <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+              </span>
+            </Link>
           </li>
           <li>
-          <a href='#'>
-            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              Blog
-              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
-            </span>
-            </a>
-          </li>
-          <li>
-          <a href='#'>
-            <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
-              Contact
-              <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
-            </span>
-          </a>
+            <Link to='/contact'>
+              <span className="w-full text-lg relative hover:text-designColor duration-300 group cursor-pointer">
+                Contact
+                <span className="w-full h-[1px] bg-designColor inline-flex absolute left-0 -bottom-1 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-300"></span>
+              </span>
+            </Link>
           </li>
         </ul>
       </div>
     </div>
   );
-}
+};
 
-export default Footer
+export default Footer;
