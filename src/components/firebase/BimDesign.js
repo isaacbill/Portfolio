47227727baef550115da224dba64204a4eded6b5
isaac bill <isaacbill247@gmail.com>
2024-07-25 22:28:35 +0300

    responsive

--- src/components/firebase/BimDesign.js
+++ src/components/firebase/BimDesign.js
@@ -56,45 +56,47 @@ const BimDesign = () => {
   }
 
   return (
-    <div className="w-full lgl:w-1/2 flex flex-col justify-center items-center relative">
+    <div className="w-full lgl:w-1/2 flex flex-col justify-center items-center relative p-4">
       <h2 className="text-2xl font-bold mb-4 text-red-600">BIM DESIGN AND MODELLING COURSE</h2>
       <p className="text-lg text-white-700 mb-4">
-      I offer BIM Design and Modelling training using Graphisoft Archicad. <a href="/contact" className="text-blue-600">Contact me</a> and let me help refine your BIM Design and Modelling skills. You can download the training guide below..
+        I offer BIM Design and Modelling training using Graphisoft Archicad. <a href="/contact" className="text-blue-600">Contact me</a> and let me help refine your BIM Design and Modelling skills. You can download the training guide below.
       </p>
-      <table className="min-w-full divide-y divide-gray-200 shadow-lg rounded-lg">
-        <thead className="bg-bodyColor">
-          <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Files</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"></th>
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
-          {filteredFiles.map(file => (
-            <tr key={file.id} className="bg-bodyColor">
-              <td className="px-6 py-4 whitespace-nowrap">
-                <a
-                  href={file.url}
-                  className="text-blue-600 underline break-words cursor-pointer"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {file.fileName}
-                </a>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <div className="flex gap-4">
-                  <button
-                    onClick={() => downloadFile(file.fileName, file.url)}
-                    className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600"
-                  >
-                    Download
-                  </button>
-                </div>
-              </td>
+      <div className="w-full overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200 shadow-lg rounded-lg">
+          <thead className="bg-bodyColor">
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Files</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {filteredFiles.map(file => (
+              <tr key={file.id} className="bg-bodyColor">
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <a
+                    href={file.url}
+                    className="text-blue-600 underline break-words cursor-pointer"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {file.fileName}
+                  </a>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="flex gap-4">
+                    <button
+                      onClick={() => downloadFile(file.fileName, file.url)}
+                      className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600"
+                    >
+                      Download
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/components/firebase/StructuralTraining.js
+++ src/components/firebase/StructuralTraining.js
@@ -56,45 +56,47 @@ const StructuralTraining = () => {
   }
 
   return (
-    <div className="w-full lgl:w-1/2 flex flex-col justify-center items-center relative p-4 bg-gray-100">
+    <div className="w-full lgl:w-3/4 flex flex-col justify-center items-center relative p-4 bg-gray-100">
       <h2 className="text-2xl font-bold mb-4 text-red-600">STRUCTURAL DESIGN TRAINING</h2>
       <p className="text-lg text-gray-700 mb-4">
-      I offer structural design training using Midas Gen & AutoCAD. <a href="/contact" className="text-blue-600">Contact me</a> and let me help refine your structural design skills. You can download the training guide below.
+        I offer structural design training using Midas Gen & AutoCAD. <a href="/contact" className="text-blue-600">Contact me</a> and let me help refine your structural design skills. You can download the training guide below.
       </p>
-      <table className="min-w-full divide-y divide-gray-200 bg-white shadow-lg rounded-lg">
-        <thead className="bg-gray-50">
-          <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Files</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {filteredFiles.map(file => (
-            <tr key={file.id} className="bg-gray-50">
-              <td className="px-6 py-4 whitespace-nowrap">
-                <a
-                  href={file.url}
-                  className="text-blue-600 underline break-words cursor-pointer"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {file.fileName}
-                </a>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <div className="flex gap-4">
-                  <button
-                    onClick={() => downloadFile(file.fileName, file.url)}
-                    className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600"
-                  >
-                    Download
-                  </button>
-                </div>
-              </td>
+      <div className="w-full overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200 bg-white shadow-lg rounded-lg">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Files</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {filteredFiles.map(file => (
+              <tr key={file.id} className="bg-gray-50">
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <a
+                    href={file.url}
+                    className="text-blue-600 underline break-words cursor-pointer"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {file.fileName}
+                  </a>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="flex gap-4">
+                    <button
+                      onClick={() => downloadFile(file.fileName, file.url)}
+                      className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600"
+                    >
+                      Download
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
